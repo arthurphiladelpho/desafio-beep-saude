c146d850667d7885a758613e937bd39bdd500d95
Arthur Philadelpho <arthur22rj@gmail.com>
2019-03-01 00:49:45 -0300

    Refactor styles and createGraph function.

--- app/javascript/packs/highcharts.js
+++ app/javascript/packs/highcharts.js
@@ -1,9 +1,10 @@
-// Essa funçao vai pegar a data e transformar no string que eu preciso pra acessar o API.
+// Essa funçao vai pegar a data e transformar no string que é preciso pra acessar o API.
 function grabDates(daysBefore){
   const today = new Date();
   let our_day = new Date(today);
   our_day.setDate(today.getDate() - daysBefore);
   let dd = our_day.getDate();
+  // O dia precisa ser definido com 2 digitos.
   if (dd < 10){
     dd = '0' + dd.toString();
   } else {
@@ -22,7 +23,7 @@ function grabDates(daysBefore){
   return date;
 }
 
-// Essa função vai ajudar a organizar nossos resultados em ordem quando usarmos o sort().
+// Esta função vai ajudar a organizar os resultados em ordem quando usarmos o sort().
 function compare(a,b) {
   if (a['date'] < b['date']){
     return -1;
@@ -31,14 +32,54 @@ function compare(a,b) {
   }
 }
 
-// Criei um array vazio pra guardar todas as informações.
+function createGraph(sigla, moeda, results) {
+  // Criar um array pra cada tipo de informação que queremos.
+    // Deste jeito será mais fácil criar o gráfico.
+  const dates = [];
+  const cur = [];
+
+  // Organizar os resultados em ordem.
+  results.sort(compare);
+
+  // Colocar as informações em seus respectivos arrays.
+  results.forEach((element) =>{
+    dates.push(element['date']);
+    cur.push(element[sigla]);
+  })
+
+  // Criar o gráfico e o inserimos no div com id de content.
+  const myChart = Highcharts.chart('content', {
+      chart: {
+          height: '35%',
+          type: 'line'
+      },
+      title: {
+          text: 'Cotação'
+      },
+      xAxis: {
+          categories: dates
+      },
+      yAxis: {
+          title: {
+              text: 'Valor'
+          }
+      },
+      series: [{
+          name: moeda,
+          data: cur
+      }]
+  });
+}
+
+// Criei um array vazio pra guardar os resultados da chamada ao API da Currency Layer.
 let results = [];
 
-// Chamamos a API 7 vezes, para pegarmos a cotação de hoje e dos 6 dias anteriores.
+// Chamar a API 7 vezes, para pegar a cotação de hoje e dos 6 dias anteriores.
 for (let i = 0; i <= 6; i++){
   fetch('http://apilayer.net/api/historical?access_key=3e43389a7ab7a1fe62bfeb0514ac8239&date='+ grabDates(i))
   .then(response => response.json())
   .then((data) => {
+    // Guardar as informações relevantes em um objeto.
     let res = {};
     res['date'] = data.date;
     res['brl'] = data.quotes.USDBRL;
@@ -50,121 +91,16 @@ for (let i = 0; i <= 6; i++){
 
 // Assim que clicar o botão com a ID de #brl, o gráfico com as cotações do real será gerado.
 const brlButton = document.querySelector('#brl');
-brlButton.addEventListener('click', function () {
-  // Criar um array pra cada tipo de informação que queremos.
-    // Deste jeito será mais fácil criar o gráfico.
-  const dates = [];
-  const brl = [];
-
-  // Organizamos nossos resultados em ordem ascendente.
-  results.sort(compare);
-
-  // Colocar as informações corretas em seus respectivos arrays.
-  results.forEach((element) =>{
-    dates.push(element['date']);
-    brl.push(element['brl']);
-  })
-
-  // Criar o gráfico e o inserimos no div com id de content.
-  const myChart = Highcharts.chart('content', {
-      chart: {
-          height: '35%',
-          type: 'line'
-      },
-      title: {
-          text: 'Cotações'
-      },
-      xAxis: {
-          categories: dates
-      },
-      yAxis: {
-          title: {
-              text: 'Valor'
-          }
-      },
-      series: [{
-          name: 'Real',
-          data: brl
-      }]
-  });
+brlButton.addEventListener('click', () => {
+  createGraph('brl', 'Real', results);
 });
 
 const eurButton = document.querySelector('#eur');
-eurButton.addEventListener('click', function () {
-  // Criar um array pra cada tipo de informação que queremos.
-    // Deste jeito será mais fácil criar o gráfico.
-  const dates = [];
-  const eur = [];
-
-  // Organizamos nossos resultados em ordem ascendente.
-  results.sort(compare);
-
-  // Colocar as informações corretas em seus respectivos arrays.
-  results.forEach((element) =>{
-    dates.push(element['date']);
-    eur.push(element['eur']);
-  })
-
-  // Criar o gráfico e o inserimos no div com id de content.
-  const myChart = Highcharts.chart('content', {
-      chart: {
-          height: '35%',
-          type: 'line'
-      },
-      title: {
-          text: 'Cotações'
-      },
-      xAxis: {
-          categories: dates
-      },
-      yAxis: {
-          title: {
-              text: 'Valor'
-          }
-      },
-      series: [{
-          name: 'Euro',
-          data: eur
-      }]
-  });
+eurButton.addEventListener('click', () => {
+  createGraph('eur', 'Euro', results);
 });
 
 const arsButton = document.querySelector('#ars');
-arsButton.addEventListener('click', function () {
-  // Criar um array pra cada tipo de informação que queremos.
-    // Deste jeito será mais fácil criar o gráfico.
-  const dates = [];
-  const ars = [];
-
-  // Organizamos nossos resultados em ordem ascendente.
-  results.sort(compare);
-
-  // Colocar as informações corretas em seus respectivos arrays.
-  results.forEach((element) =>{
-    dates.push(element['date']);
-    ars.push(element['ars']);
-  })
-
-  // Criar o gráfico e o inserimos no div com id de content.
-  const myChart = Highcharts.chart('content', {
-      chart: {
-          height: '35%',
-          type: 'line'
-      },
-      title: {
-          text: 'Cotações'
-      },
-      xAxis: {
-          categories: dates
-      },
-      yAxis: {
-          title: {
-              text: 'Valor'
-          }
-      },
-      series: [{
-          name: 'Peso Argentino',
-          data: ars
-      }]
-  });
+arsButton.addEventListener('click', () => {
+  createGraph('ars', 'Peso Argentino', results);
 });
