8af8de29b25bb5f5fce0edca10accbf42b18c488
Arthur Philadelpho <arthur22rj@gmail.com>
2019-03-01 00:15:24 -0300

    Add buttons and fix grabDates function.

--- app/javascript/packs/highcharts.js
+++ app/javascript/packs/highcharts.js
@@ -1,21 +1,23 @@
 // Essa funçao vai pegar a data e transformar no string que eu preciso pra acessar o API.
 function grabDates(daysBefore){
   const today = new Date();
-  let dd = today.getDate() - daysBefore;
-  // O dia precisa ser definido com 2 digitos.
+  let our_day = new Date(today);
+  our_day.setDate(today.getDate() - daysBefore);
+  let dd = our_day.getDate();
   if (dd < 10){
     dd = '0' + dd.toString();
   } else {
     dd.toString();
   }
-  let mm = today.getMonth() + 1; //Janeiro é 0.
+  dd.toString();
+  let mm = our_day.getMonth() + 1; //Janeiro é 0.
   // O mês precisa ser definido com 2 digitos.
   if (mm < 10){
     mm = '0' + mm.toString();
   } else {
     mm.toString();
   }
-  const yyyy = today.getFullYear().toString();
+  const yyyy = our_day.getFullYear().toString();
   const date = yyyy + '-' + mm + '-' + dd;
   return date;
 }
@@ -46,15 +48,13 @@ for (let i = 0; i <= 6; i++){
   });
 }
 
-// Assim que clicar o botão com a ID de #clickme, o gráfico com as cotações será gerado.
-const button = document.querySelector('#clickme');
-button.addEventListener('click', function () {
+// Assim que clicar o botão com a ID de #brl, o gráfico com as cotações do real será gerado.
+const brlButton = document.querySelector('#brl');
+brlButton.addEventListener('click', function () {
   // Criar um array pra cada tipo de informação que queremos.
     // Deste jeito vai ser mais fácil para criar o gráfico.
   const dates = [];
   const brl = [];
-  const eur = [];
-  const ars = [];
 
   // Organizamos nossos resultados em ordem ascendente.
   results.sort(compare);
@@ -63,14 +63,12 @@ button.addEventListener('click', function () {
   results.forEach((element) =>{
     dates.push(element['date']);
     brl.push(element['brl']);
-    eur.push(element['eur']);
-    ars.push(element['ars']);
   })
 
   // Criar o gráfico e o inserimos no div com id de content.
   const myChart = Highcharts.chart('content', {
       chart: {
-          height: '40%',
+          height: '35%',
           type: 'line'
       },
       title: {
@@ -87,10 +85,84 @@ button.addEventListener('click', function () {
       series: [{
           name: 'Real',
           data: brl
-      }, {
+      }]
+  });
+});
+
+const eurButton = document.querySelector('#eur');
+eurButton.addEventListener('click', function () {
+  // Criar um array pra cada tipo de informação que queremos.
+    // Deste jeito vai ser mais fácil para criar o gráfico.
+  const dates = [];
+  const eur = [];
+
+  // Organizamos nossos resultados em ordem ascendente.
+  results.sort(compare);
+
+  // Colocar as informações corretas em seus respectivos arrays.
+  results.forEach((element) =>{
+    dates.push(element['date']);
+    eur.push(element['eur']);
+  })
+
+  // Criar o gráfico e o inserimos no div com id de content.
+  const myChart = Highcharts.chart('content', {
+      chart: {
+          height: '35%',
+          type: 'line'
+      },
+      title: {
+          text: 'Cotações'
+      },
+      xAxis: {
+          categories: dates
+      },
+      yAxis: {
+          title: {
+              text: 'Valor'
+          }
+      },
+      series: [{
           name: 'Euro',
           data: eur
-      }, {
+      }]
+  });
+});
+
+const arsButton = document.querySelector('#ars');
+arsButton.addEventListener('click', function () {
+  // Criar um array pra cada tipo de informação que queremos.
+    // Deste jeito vai ser mais fácil para criar o gráfico.
+  const dates = [];
+  const ars = [];
+
+  // Organizamos nossos resultados em ordem ascendente.
+  results.sort(compare);
+
+  // Colocar as informações corretas em seus respectivos arrays.
+  results.forEach((element) =>{
+    dates.push(element['date']);
+    ars.push(element['ars']);
+  })
+
+  // Criar o gráfico e o inserimos no div com id de content.
+  const myChart = Highcharts.chart('content', {
+      chart: {
+          height: '35%',
+          type: 'line'
+      },
+      title: {
+          text: 'Cotações'
+      },
+      xAxis: {
+          categories: dates
+      },
+      yAxis: {
+          title: {
+              text: 'Valor'
+          }
+      },
+      series: [{
           name: 'Peso Argentino',
           data: ars
       }]
